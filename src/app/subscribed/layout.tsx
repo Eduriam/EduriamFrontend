b6f1f7af5d28da9853677c056ccc66dbfe18ead2
Jan Jeníček <11.honzik@gmail.com>
2026-03-17 11:44:45 +0100

    feat: :sparkles: Update subscribed page design

--- src/app/subscribed/layout.tsx
+++ src/app/subscribed/layout.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { Container } from "@mui/material";
-
 import ProtectedRoute from "components/layouts/authentication/ProtectedRoute/ProtectedRoute";
 
 export interface ILayout {
@@ -9,16 +7,7 @@ export interface ILayout {
 }
 
 const Layout: React.FC<ILayout> = ({ children }) => {
-  return (
-    <ProtectedRoute>
-      <Container
-        maxWidth="sm"
-        sx={{ justifyContent: "center", display: "flex" }}
-      >
-        {children}
-      </Container>
-    </ProtectedRoute>
-  );
+  return <ProtectedRoute>{children}</ProtectedRoute>;
 };
 
 export default Layout;
